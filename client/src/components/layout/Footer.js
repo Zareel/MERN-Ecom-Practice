9596f82fe9475ec695b502d020ed06be999ef178
Zareel Kalam <zareelbwd@gmail.com>
2023-05-13 17:44:06 +0530

    logo added, theme change

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,25 +4,26 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="bg-gray-900 text-white py-4  ">
-      <h1 className="p-2 dark:text-violet-400 text-3xl text-center hover:text-violet-500 active:text-violet-700 ">
-        Click to Cart
-      </h1>
+      <div className="w-full flex justify-center">
+        <img className="w-[150px] mx-auto" src="/images/logo.png" alt="logo" />
+      </div>
+
       <div className="w-full flex items-center justify-center">
         <Link
           to="/about"
-          className=" p-2 dark:text-violet-400 text-lg text-center hover:text-violet-500 active:text-violet-700"
+          className=" p-2 text-green-500 text-lg text-center hover:text-green-600 active:text-green-700"
         >
           About
         </Link>
         <Link
           to="/contact"
-          className=" p-2 dark:text-violet-400 text-lg text-center hover:text-violet-500 active:text-violet-700"
+          className=" p-2 text-green-500 text-lg text-center hover:text-green-600 active:text-green-700"
         >
           Contact
         </Link>
         <Link
           to="/policy"
-          className=" p-2 dark:text-violet-400 text-lg text-center hover:text-violet-500 active:text-violet-700"
+          className=" p-2 text-green-500 text-lg text-center hover:text-green-600 active:text-green-700"
         >
           Privacy Policy
         </Link>
